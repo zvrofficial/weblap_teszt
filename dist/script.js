dbd788ac9ccc9eeb2bf766323c4a5b2119b1cfb3
Bendeguz <kovago.bendeguz234@gmail.com>
2021-01-05 13:20:38 +0100

    elso

---
+++ dist/script.js
@@ -0,0 +1,90 @@
+// Sliders
+
+var slider = document.getElementById('song-progress');
+
+noUiSlider.create(slider, {
+	start: [ 20 ],
+	range: {
+		'min': [   0 ],
+		'max': [ 100 ]
+	}
+});
+
+var slider = document.getElementById('song-volume');
+
+noUiSlider.create(slider, {
+	start: [ 90 ],
+	range: {
+		'min': [   0 ],
+		'max': [ 100 ]
+	}
+});
+
+
+// Tooltips
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Viewport Heights
+
+$(window).on("resize load", function(){
+  
+  var totalHeight = $(window).height();
+
+  var headerHeight = $('.header').outerHeight();
+  var footerHeight = $('.current-track').outerHeight();
+  var playlistHeight = $('.playlist').outerHeight();
+  var nowPlaying = $('.playing').outerHeight();
+
+  var navHeight = totalHeight - (headerHeight + footerHeight + playlistHeight + nowPlaying);
+  var artistHeight = totalHeight - (headerHeight + footerHeight);
+
+  console.log(totalHeight);
+  
+  $(".navigation").css("height" , navHeight);
+  $(".artist").css("height" , artistHeight);
+  $(".social").css("height" , artistHeight);
+  
+});
+    
+
+
+  
+
+// Collapse Toggles
+
+$(".navigation__list__header").on( "click" , function() {
+  
+  $(this).toggleClass( "active" );
+  
+});
+
+new Twitch.Embed("twitch-embed", {
+								width: 854,
+									height: 480,
+								channel: "novapapa",
+
+
+// Media Queries
+
+$(window).on("resize load", function(){
+	if ($(window).width() <= 768){	
+		
+    $(".collapse").removeClass("in");
+    
+    $(".navigation").css("height" , "auto");
+    
+    $(".artist").css("height" , "auto");
+    
+	}	
+});
+
+$(window).on("resize load", function(){
+	if ($(window).width() > 768){	
+		
+    $(".collapse").addClass("in");
+    
+	}	
+});
